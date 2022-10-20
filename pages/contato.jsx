c535fdc1e7a1a54bffc17fd4ad8ce680435ae274
jlbavaresco@gmail.com <jlbavaresco@gmail.com>
2022-10-20 08:36:48 -0300

    Testando variavel de ambiente

--- pages/contato.jsx
+++ pages/contato.jsx
@@ -2,6 +2,7 @@ export default function contato(){
     return (
         <div>
             <h1>Página de contato</h1>
+            <h2>{process.env.NEXT_PUBLIC_API_URL}</h2>
         </div>
     )
 }
